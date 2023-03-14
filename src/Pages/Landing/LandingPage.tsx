f0403c98e4246a81671449167f36b408ca908ffc
VasaOfficial <VasaPofficial@gmail.com>
2023-03-14 17:37:33 +0100

    fix: change scroll feat so that when on home page it cant scroll down and if on the last page it cant go down only up

--- src/Pages/Landing/LandingPage.tsx
+++ src/Pages/Landing/LandingPage.tsx
@@ -9,7 +9,8 @@ import Contact from "../../components/ScrollablePages/Contact";
 
 function LandingPage() {
   const [menuOpen, setMenuOpen] = useState(false);
-
+  
+  // changing pages on scroll
   useEffect(() => {
     const sections = [
       document.getElementById("section-1"),
@@ -22,10 +23,20 @@ function LandingPage() {
     const handleWheel = (event: WheelEvent) => {
       const delta = Math.sign(event.deltaY);
       sections[currentIndex]?.classList.remove("is-active");
-      currentIndex = (currentIndex + delta + sections.length) % sections.length;
+    
+      if (currentIndex === 2 && delta === 1) {
+        // Do not allow scrolling down on section-3
+        currentIndex = 2;
+      } else if (currentIndex === 0 && delta === -1) {
+        // Do not allow scrolling up on section-1
+        currentIndex = 0;
+      } else {
+        currentIndex = (currentIndex + delta + sections.length) % sections.length;
+      }
+    
       sections[currentIndex]?.classList.add("is-active");
     };
-  
+    
     sections[currentIndex]?.classList.add("is-active");
     sections.forEach((section) => {
       section?.addEventListener("wheel", handleWheel);
@@ -37,6 +48,7 @@ function LandingPage() {
       });
     };
   }, []);
+  
   
   // opening/closing navbar menu
   const toggleMenu = () => {
