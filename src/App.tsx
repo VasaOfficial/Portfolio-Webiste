f97d25e05cea323d43c5cbffd7fafddeabaeb807
VasaOfficial <VasaPofficial@gmail.com>
2023-03-23 19:33:47 +0100

    build: add a spinner if the page is loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { useState, useEffect } from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Triangle } from 'react-loader-spinner'
 import LandingPage from './Pages/Landing';
 
 export function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  
+    return () => clearTimeout(timeout);
+  }, []);
+  
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-      </Routes>
+      {loading && <Triangle height="80" width="80" color="#eab308" ariaLabel="triangle-loading" wrapperClass="spinner" visible />}
+      {!loading && (
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      )}
     </div>
   );
 }
